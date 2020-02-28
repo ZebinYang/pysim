22d8fe78934d07ac8f60a7c51f22a0872d3662f0
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-02-28 13:33:45 +0800

    update

--- pysim/aspline.py
+++ pysim/aspline.py
@@ -57,11 +57,11 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
         w = np.ones([self.knot_num], dtype=np.float32) 
         W = np.diag(w)
 
-        BB = basis.T.dot(sample_weight.reshape([-1, 1])).dot(basis)
+        BB = basis.T.dot(np.diag(sample_weight)).dot(basis)
         for i in range(self.maxiter):
             U = cholesky(BB + self.reg_gamma * np.dot(np.dot(D.T, W), D))
             M = scipy.linalg.lapack.clapack.dtrtri(U)[0]
-            update_a = np.dot(np.dot(M, M.T.conj()), basis.T.dot(sample_weight.reshape([-1, 1])).dot(y))
+            update_a = np.dot(np.dot(M, M.T.conj()), basis.T.dot(np.diag(sample_weight)).dot(y))
             # The original implementation of matrix inversion is very slow and so it is commented. 
             # update_a = np.dot(np.linalg.inv(np.dot(basis.T, basis) + self.reg_gamma * np.dot(np.dot(D.T, W), D)), np.dot(basis.T, y))
             update_w = 1 / (np.dot(D, update_a) ** 2 + self.epsilon ** 2)
