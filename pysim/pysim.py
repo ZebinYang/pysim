d7e53c24c49c9ee2b44423a83168ab4e1a36bd2a
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-03-02 14:06:44 +0800

    update

--- pysim/pysim.py
+++ pysim/pysim.py
@@ -133,12 +133,12 @@ class BaseSIM(BaseEstimator, metaclass=ABCMeta):
                 active_beta.append(beta)
                 active_beta_inx.append(idx)
 
-        rects = ax.barh(np.arange(len(active_beta)), [beta for beta,_ in sorted(zip(active_beta, active_beta_inx))])
-        ax.set_yticks(np.arange(len(active_beta)))
-        ax.set_yticklabels(["X" + str(idx + 1) for _, idx in sorted(zip(active_beta, active_beta_inx))])
+        rects = ax2.barh(np.arange(len(active_beta)), [beta for beta,_ in sorted(zip(active_beta, active_beta_inx))])
+        ax2.set_yticks(np.arange(len(active_beta)))
+        ax2.set_yticklabels(["X" + str(idx + 1) for _, idx in sorted(zip(active_beta, active_beta_inx))])
         for rect in rects:
             _, height = rect.get_xy()
-            ax.text(rect.get_x() + rect.get_width() + 0.005, height + 0.3,
+            ax2.text(rect.get_x() + rect.get_width() + 0.005, height + 0.3,
                     "%0.3f" % (rect.get_width()))
         ax2.set_xlim(0, np.max(active_beta) + 0.05)
         ax2.set_ylim(-1, len(active_beta_inx))
