37be04dd412a76f6ec71cf6c02154bab32bc4f4e
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-03-03 21:22:59 +0800

    update

--- pysim/aspline.py
+++ pysim/aspline.py
@@ -3,34 +3,19 @@ import numpy as np
 import pandas as pd 
 from scipy.linalg import cholesky
 from matplotlib import pyplot as plt
+from abc import ABCMeta, abstractmethod
 
 from sklearn.utils.validation import check_is_fitted
 from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
 from patsy import dmatrix, build_design_matrices
 
-def diff_matrix(order, knot_num):
-    results = [] # a container to collect the rows
-    n_rows = order + 2
-    for _ in range(n_rows): 
-        row = [1] # a starter 1 in the row
-        if results: # then we're in the second row or beyond
-            last_row = results[-1] # reference the previous row
-            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
-            row.append(1)
-        results.append(row) # add the row to the results.
 
-    diss_operator = [] # a container to collect the rows
-    for i, item in enumerate(np.array(row)):
-        diss_operator.append(item * (-1) ** i)
-    diss_operator.reverse()
-    D = np.zeros((knot_num, knot_num + order + 1), dtype=np.float32)
-    for i in range(knot_num):
-        D[i,i:(i+order+2)] = diss_operator
-    return D
+class BaseASpline(BaseEstimator, metaclass=ABCMeta):
+    """
+        Base class for ASpline classification and regression.
+     """
 
-    
-class ASplineRegressor(BaseEstimator, RegressorMixin):
-
+    @abstractmethod
     def __init__(self, knot_num=20, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99, maxiter=10):
 
         self.knot_num = knot_num
@@ -42,10 +27,114 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
         self.threshold = threshold
         self.maxiter = maxiter
 
-    def get_loss(self, label, pred):
+    @staticmethod
+    def diff_matrix(order, knot_num):
+        results = [] # a container to collect the rows
+        n_rows = order + 2
+        for _ in range(n_rows): 
+            row = [1] # a starter 1 in the row
+            if results: # then we're in the second row or beyond
+                last_row = results[-1] # reference the previous row
+                row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+                row.append(1)
+            results.append(row) # add the row to the results.
+
+        diss_operator = [] # a container to collect the rows
+        for i, item in enumerate(np.array(row)):
+            diss_operator.append(item * (-1) ** i)
+        diss_operator.reverse()
+        D = np.zeros((knot_num, knot_num + order + 1), dtype=np.float32)
+        for i in range(knot_num):
+            D[i,i:(i+order+2)] = diss_operator
+        return D
+
+    def _validate_hyperparameters(self):
+        
+        if not isinstance(self.degree, int):
+            raise ValueError("degree must be an integer, got %s." % self.degree)
+
+        if self.degree < 0:
+            raise ValueError("degree must be >= 0, got" % self.degree)
+        
+        if not isinstance(self.knot_num, int):
+            raise ValueError("knot_num must be an integer, got %s." % self.knot_num)
+
+        if self.knot_num <= 0:
+            raise ValueError("knot_num must be > 0, got" % self.knot_num)
+
+        if self.reg_lambda <= 0:
+            raise ValueError("reg_lambda must be > 0, got %s." % self.reg_lambda)
+
+        if self.reg_gamma <= 0:
+            raise ValueError("reg_gamma must be > 0, got %s." % self.reg_gamma)
+
+        if self.xmin >= self.xmax:
+            raise ValueError("xmin must be < xmax, got %s and %s." % (self.xmin, self.xmax))
+
+        if self.epsilon <= 0:
+            raise ValueError("epsilon must be > 0, got %s." % self.epsilon)
+
+        if self.threshold <= 0:
+            raise ValueError("threshold must be > 0, got %s." % self.threshold)
+
+        if not isinstance(self.maxiter, int):
+            raise ValueError("maxiter must be an integer, got %s." % self.maxiter)
+
+        if self.maxiter <= 0:
+            raise ValueError("maxiter must be > 0, got" % self.maxiter)
+
+            
+    def visualize(self):
+
+        check_is_fitted(self, "coef_")
+
+        fig = plt.figure(figsize=(6, 4))
+        xgrid = np.linspace(self.xmin, self.xmax, 100).reshape([-1, 1])
+        ygrid = self.shape_fit_.predict(xgrid)
+        plt.plot(xgrid, ygrid)
+        plt.title("Shape Function", fontsize=12)
+        plt.show()
+
+    def decision_function(self, x):
+
+        check_is_fitted(self, "coef_")
+        x = x.copy()
+        x[x < self.xmin] = self.xmin
+        x[x > self.xmax] = self.xmax
+        design_matrix = np.asarray(build_design_matrices([self.selected_xphi_.design_info],
+                                         {"x": x, "knots": self.selected_knots_, "degree": self.degree})[0])
+        pred = np.dot(design_matrix, self.coef_)
+        return pred
+
+
+class ASplineRegressor(BaseASpline, RegressorMixin):
+
+    def __init__(self, knot_num=20, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99, maxiter=10):
+
+        super(ASplineRegressor, self).__init__(knot_num=knot_num
+                                reg_gamma=reg_gamma
+                                xmin=xmin
+                                xmax=xmax
+                                degree=degree
+                                epsilon=epsilon
+                                threshold=threshold
+                                maxiter=maxiter)
+
+    @staticmethod
+    def _get_loss(label, pred):
         return - np.mean((label - pred) ** 2)
 
+    def _validate_input(self, x, y):
+        x, y = check_X_y(x, y, accept_sparse=['csr', 'csc', 'coo'],
+                         multi_output=True, y_numeric=True)
+        if y.ndim == 2 and y.shape[1] == 1:
+            y = column_or_1d(y, warn=False)
+        return x, y
+
     def fit(self, x, y, sample_weight=None):
+
+        self._validate_hyperparameters()
+        x, y = self._validate_input(x, y)
 
         n_samples = x.shape[0]
         if sample_weight is None:
@@ -59,7 +148,7 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
                    {"x": x, "knots": knots, "degree": self.degree})[0])
 
         best_loss = np.inf
-        D = diff_matrix(self.degree, self.knot_num)
+        D = self.diff_matrix(self.degree, self.knot_num)
         update_w = np.ones([self.knot_num, 1], dtype=np.float32) 
         BWB = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), init_basis, axes=([0], [0]))
         BWY = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), y, axes=([0], [0]))
@@ -68,7 +157,7 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
             U = cholesky(BWB + self.reg_gamma * DwD)
             M = scipy.linalg.lapack.clapack.dtrtri(U)[0]
             update_a_temp = np.dot(np.dot(M, M.T.conj()), BWY)
-            new_loss = self.get_loss(y, np.dot(init_basis, update_a_temp))
+            new_loss = self._get_loss(y, np.dot(init_basis, update_a_temp))
             if new_loss - best_loss >= 0:
                 break
             best_loss = new_loss
@@ -87,47 +176,60 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
 
     def predict(self, x):
 
-        check_is_fitted(self, "coef_")
-        x = x.copy()
-        x[x < self.xmin] = self.xmin
-        x[x > self.xmax] = self.xmax
-        design_matrix = np.asarray(build_design_matrices([self.selected_xphi_.design_info],
-                                  {"x": x, "knots": self.selected_knots_, "degree": self.degree})[0])
-        pred = np.dot(design_matrix, self.coef_)
+        pred = self.decision_function(x)
         return pred
     
 
-class ASplineClassifier(BaseEstimator, ClassifierMixin):
+class ASplineClassifier(BaseASpline, ClassifierMixin):
 
     def __init__(self, knot_num=20, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99,
                  maxiter=10, maxiter_irls=10):
 
-        self.knot_num = knot_num
-        self.reg_gamma = reg_gamma
-        self.xmin = xmin
-        self.xmax = xmax
-        self.degree = degree
-        self.epsilon = epsilon
-        self.threshold = threshold
-        self.maxiter = maxiter
+        super(ASplineClassifier, self).__init__(knot_num=knot_num
+                                reg_gamma=reg_gamma
+                                xmin=xmin
+                                xmax=xmax
+                                degree=degree
+                                epsilon=epsilon
+                                threshold=threshold
+                                maxiter=maxiter)
+
         self.maxiter_irls = maxiter_irls
         self.EPS = 10**(-8)
 
-    def link(self, x):
+    @staticmethod
+    def _link(x):
         with np.errstate(divide='ignore', over='ignore'):
             return 1 / (1 + np.exp(-x))
 
-    def inv_link(self, x):
+    @staticmethod
+    def _inv_link(x):
         with np.errstate(divide='ignore', over='ignore'):
             return np.log(x) - np.log(1 - x)
     
-    def get_loss(self, label, pred):
+    @staticmethod
+    def _get_loss(label, pred):
         with np.errstate(divide='ignore', over='ignore'):
             pred = np.clip(pred, self.EPS, 1. - self.EPS)
             return - np.mean(label * np.log(pred) + (1 - label) * np.log(1 - pred))
-        
+       
+    def _validate_input(self, x, y):
+        x, y = check_X_y(x, y, accept_sparse=['csr', 'csc', 'coo'],
+                         multi_output=True)
+        if y.ndim == 2 and y.shape[1] == 1:
+            y = column_or_1d(y, warn=False)
+
+        self._label_binarizer = LabelBinarizer()
+        self._label_binarizer.fit(y)
+        self.classes_ = self._label_binarizer.classes_
+
+        y = self._label_binarizer.transform(y) * 1.0
+        return x, y
+
     def fit(self, x, y, sample_weight=None):
 
+        self._validate_hyperparameters()
+        x, y = self._validate_input(x, y)
         n_samples = x.shape[0]
         if sample_weight is None:
             sample_weight = np.ones(n_samples)
@@ -143,18 +245,18 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
         tempy = y.copy().astype(np.float32)
         tempy[tempy==0] = 0.01
         tempy[tempy==1] = 0.99
-        D = diff_matrix(self.degree, self.knot_num)
+        D = self.diff_matrix(self.degree, self.knot_num)
         update_w = np.ones([self.knot_num, 1], dtype=np.float32) 
         DwD = np.tensordot(D * update_w.reshape([-1, 1]), D, axes=([0], [0]))
         BWB = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), init_basis, axes=([0], [0]))
-        BWY = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), self.inv_link(tempy), axes=([0], [0]))
+        BWY = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), self._inv_link(tempy), axes=([0], [0]))
         update_a = np.dot(np.linalg.pinv(BWB + self.reg_gamma * DwD), BWY)
-        best_loss = self.get_loss(y, self.link(np.dot(init_basis, update_a)))
+        best_loss = self._get_loss(y, self._link(np.dot(init_basis, update_a)))
         for i in range(self.maxiter):
             best_loss_irls = np.inf
             for j in range(self.maxiter_irls):
                 lp = np.dot(init_basis, update_a)
-                mu = self.link(lp)
+                mu = self._link(lp)
                 omega = mu * (1 - mu)
                 mask = (np.abs(omega) >= self.EPS) * np.isfinite(omega)
                 mask = mask.ravel()
@@ -166,7 +268,7 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
                 BWOB = np.tensordot(BW * omega[mask].reshape([-1, 1]), init_basis[mask, :], axes=([0], [0]))
                 update_a_temp = np.dot(np.linalg.pinv(BWOB + self.reg_gamma * DwD),
                                 BWOB.dot(update_a) + np.tensordot(BW, y[mask] - mu[mask], axes=([0], [0])))
-                new_loss = self.get_loss(y, self.link(np.dot(init_basis, update_a_temp)))
+                new_loss = self._get_loss(y, self._link(np.dot(init_basis, update_a_temp)))
                 if new_loss - best_loss_irls >= 0:
                     break
                 best_loss_irls = new_loss
@@ -184,7 +286,7 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
                           {"x": x, "knots": self.selected_knots_, "degree": self.degree})[0])
 
         seBWB = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), selected_basis, axes=([0], [0]))
-        seBWY = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), self.inv_link(tempy), axes=([0], [0]))
+        seBWY = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), self._inv_link(tempy), axes=([0], [0]))
         self.coef_ = np.dot(np.linalg.pinv(seBWB), seBWY)
         for j in range(self.maxiter_irls):
             lp = np.dot(selected_basis, self.coef_)
@@ -200,13 +302,13 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
                           seBWOB.dot(self.coef_) + np.tensordot(seBW, y[mask] - mu[mask], axes=([0], [0])))
         return self
     
+    def predict_proba(self, x):
+
+        pred = self.decision_function(x)
+        pred_proba = 1 / (1 + np.exp(- pred))
+        return pred_proba
+
     def predict(self, x):
 
-        check_is_fitted(self, "coef_")
-        x = x.copy()
-        x[x < self.xmin] = self.xmin
-        x[x > self.xmax] = self.xmax
-        design_matrix = np.asarray(build_design_matrices([self.selected_xphi_.design_info],
-                                         {"x": x, "knots": self.selected_knots_, "degree": self.degree})[0])
-        pred = np.dot(design_matrix, self.coef_)
-        return pred
+        pred_proba = self.predict_proba(x)
+        return self._label_binarizer.inverse_transform(pred_proba)
