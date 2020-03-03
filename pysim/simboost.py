0e233f4523320c586d4375bfb11903a068ab4563
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-03-03 19:52:57 +0800

    update

--- pysim/simboost.py
+++ pysim/simboost.py
@@ -49,20 +49,65 @@ class BaseSIMBooster(BaseEstimator, metaclass=ABCMeta):
         if self.early_stop_thres <= 0:
             raise ValueError("early_stop_thres must be > 0, got %s." % self.early_stop_thres)
 
+    @property
+    def estimator_importances_(self):
+        """Return the estimator importances (the higher, the more important the feature).
+        Returns
+        -------
+        estimator_importances_ : ndarray of shape (n_estimators,)
+            The estimator importances.
+        """
+        if self.estimators_ is None or len(self.estimators_) == 0:
+            raise ValueError("Estimator not fitted, "
+                             "call `fit` before `feature_importances_`.")
+        estimator_importance = []
+        for indice, estimator in enumerate(self.estimators_):
+
+            xgrid = np.linspace(estimator.shape_fit_.xmin, estimator.shape_fit_.xmax, 100).reshape([-1, 1])
+            ygrid = estimator.shape_fit_.predict(xgrid)
+            estimator_importance.append(np.std(ygrid))
+        importance_ratio = estimator_importance / np.sum(estimator_importance)
+        return importance_ratio
+
+    @property
+    def orthogonality_measure_(self):
+        """Return the orthogonality measure (the lower, the better).
+        Returns
+        -------
+        orthogonality_measure_ : float scalar
+        """
+        if self.estimators_ is None or len(self.estimators_) == 0:
+            raise ValueError("Estimator not fitted, "
+                             "call `fit` before `feature_importances_`.")
+        return np.linalg.norm(np.dot(self.betas_, self.betas_.T) - np.eye(self.betas_.shape[0]))
+    
+    @property
+    def projection_indices_(self):
+        """Return the projection indices.
+        Returns
+        -------
+        projection_indices_ : ndarray of shape (d, n_estimators)
+        """
+        if self.estimators_ is None or len(self.estimators_) == 0:
+            raise ValueError("Estimator not fitted, "
+                             "call `fit` before `feature_importances_`.")
+
+        return np.array([estimator.beta_.flatten() for estimator in self.estimators_])
+
     def visualize(self):
 
-        check_is_fitted(self, "sim_estimators_")
+        check_is_fitted(self, "estimators_")
 
         idx = 0
-        max_ids = len(self.sim_estimators_)
+        max_ids = len(self.estimators_)
         fig = plt.figure(figsize=(12, 4.2 * max_ids))
         outer = gridspec.GridSpec(max_ids, 1, hspace=0.2)
-        for indice, model in enumerate(self.sim_estimators_):
+        for indice, estimator in enumerate(self.estimators_):
 
             inner = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=outer[indice], wspace=0.15)
             ax1 = plt.Subplot(fig, inner[0]) 
-            xgrid = np.linspace(model.shape_fit_.xmin, model.shape_fit_.xmax, 100).reshape([-1, 1])
-            ygrid = model.shape_fit_.predict(xgrid)
+            xgrid = np.linspace(estimator.shape_fit_.xmin, estimator.shape_fit_.xmax, 100).reshape([-1, 1])
+            ygrid = estimator.shape_fit_.predict(xgrid)
             ax1.plot(xgrid, ygrid)
             if indice == 0:
                 ax1.set_title("Shape Function", fontsize=12)
@@ -73,13 +118,13 @@ class BaseSIMBooster(BaseEstimator, metaclass=ABCMeta):
             ax2 = plt.Subplot(fig, inner[1]) 
             active_beta = []
             active_beta_inx = []
-            for idx, beta in enumerate(model.beta_.ravel()):
+            for idx, beta in enumerate(estimator.beta_.ravel()):
                 if np.abs(beta) > 0:
                     active_beta.append(beta)
                     active_beta_inx.append(idx)
 
             rects = ax2.barh(np.arange(len(active_beta)),
-                        [model.beta_.ravel()[idx] for _, idx in sorted(zip(np.abs(active_beta), active_beta_inx))])
+                        [estimator.beta_.ravel()[idx] for _, idx in sorted(zip(np.abs(active_beta), active_beta_inx))])
             ax2.set_yticks(np.arange(len(active_beta)))
             ax2.set_yticklabels(["X" + str(idx + 1) for _, idx in sorted(zip(np.abs(active_beta), active_beta_inx))])
             ax2.set_xlim(np.min(active_beta) - 0.1, np.max(active_beta) + 0.1)
@@ -89,12 +134,12 @@ class BaseSIMBooster(BaseEstimator, metaclass=ABCMeta):
             fig.add_subplot(ax2)
         plt.show()
 
-    def _predict(self, x):
+    def decision_function(self, x):
 
-        check_is_fitted(self, "sim_estimators_")
+        check_is_fitted(self, "estimators_")
         
         pred = 0
-        for sim_clf in self.sim_estimators_:
+        for sim_clf in self.estimators_:
             pred += sim_clf.predict(x)
         return pred
 
@@ -117,6 +162,7 @@ class SIMBoostRegressor(BaseSIMBooster, RegressorMixin):
 
     def fit(self, x, y, sample_weight=None):
 
+        start = time.time()
         self._validate_hyperparameters()
         x, y = self._validate_input(x, y)
         n_samples, n_features = x.shape
@@ -131,31 +177,26 @@ class SIMBoostRegressor(BaseSIMBooster, RegressorMixin):
         val_fold = np.ones((len(indices)))
         val_fold[idx1] = -1
 
-        pred_train = 0 
         pred_val = 0
         z = y.ravel().copy()
 
         mse_opt = np.inf
-        self.time_cost_ = 0
-        self.sim_estimators_ = []
-        self.sim_importance_ = []
+        self.estimators_ = []
         for i in range(self.n_estimators):
 
-            # fit SIM model
+            # fit SIM estimator
             param_grid = {"method": ["second_order", 'first_order']}
             grid = GridSearchCV(SIMRegressor(degree=2, knot_num=20, spline="a_spline", reg_lambda=0.1, reg_gamma=10,
                                   random_state=self.random_state), 
                          scoring={"mse": make_scorer(mean_squared_error, greater_is_better=False)}, refit=False,
                          cv=PredefinedSplit(val_fold), param_grid=param_grid, verbose=0, error_score=np.nan)
             # time
-            start = time.time()
             grid.fit(x, z, sample_weight=sample_weight)
-            model = grid.estimator.set_params(**grid.cv_results_['params'][np.where((grid.cv_results_['rank_test_mse'] == 1))[0][0]])
-            model.fit(x[idx1, :], z[idx1], sample_weight=sample_weight[idx1])
-            self.time_cost_ += time.time() - start
+            estimator = grid.estimator.set_params(**grid.cv_results_['params'][np.where((grid.cv_results_['rank_test_mse'] == 1))[0][0]])
+            estimator.fit(x[idx1, :], z[idx1], sample_weight=sample_weight[idx1])
 
             # early stop
-            pred_val_temp = pred_val + model.predict(x[idx2, :]).reshape([-1, 1])
+            pred_val_temp = pred_val + estimator.predict(x[idx2, :]).reshape([-1, 1])
             mse_new = mean_squared_error(y[idx2], pred_val_temp)
             if mse_opt > mse_new:           
                 mse_opt = mse_new
@@ -167,27 +208,19 @@ class SIMBoostRegressor(BaseSIMBooster, RegressorMixin):
                 break
 
             # update    
-            pred_train += model.predict(x[idx1, :]).reshape([-1, 1])
-            pred_val += model.predict(x[idx2, :]).reshape([-1, 1])
-            z = z - model.predict(x)
-            
-            self.sim_estimators_.append(model)
-            xgrid = np.linspace(model.shape_fit_.xmin, model.shape_fit_.xmax, 100).reshape([-1, 1])
-            ygrid = model.shape_fit_.predict(xgrid)
-            self.sim_importance_.append(np.std(ygrid))
+            z = z - estimator.predict(x)
+            pred_val += estimator.predict(x[idx2, :]).reshape([-1, 1])
+            self.estimators_.append(estimator)
         
         self.tr_idx = idx1
         self.val_idx = idx2
-        self.importance_ratio_ = self.sim_importance_ / np.sum(self.sim_importance_)
-        self.betas_ = np.array([model.beta_.flatten() for model in self.sim_estimators_])
-        self.ortho_measure_ = np.linalg.norm(np.dot(self.betas_, self.betas_.T) - np.eye(self.betas_.shape[0]))
-
+        self.time_cost_ = time.time() - start
         return self
 
     def predict(self, x):
 
-        check_is_fitted(self, "sim_estimators_")
-        pred = self._predict(x)
+        check_is_fitted(self, "estimators_")
+        pred = self.decision_function(x)
         return pred
 
 
@@ -215,6 +248,7 @@ class SIMLogitBoostClassifier(BaseSIMBooster, ClassifierMixin):
 
     def fit(self, x, y, sample_weight=None):
 
+        start = time.time()
         self._validate_hyperparameters()
         x, y = self._validate_input(x, y)
         n_samples, n_features = x.shape
@@ -235,9 +269,7 @@ class SIMLogitBoostClassifier(BaseSIMBooster, ClassifierMixin):
         sample_weight = 1 / n_samples * np.ones(n_samples)
 
         roc_auc_opt = -np.inf
-        self.time_cost_ = 0
-        self.sim_estimators_ = []
-        self.sim_importance_ = []
+        self.estimators_ = []
         for i in range(self.n_estimators):
 
             sample_weight = probs * (1 - probs)
@@ -248,21 +280,19 @@ class SIMLogitBoostClassifier(BaseSIMBooster, ClassifierMixin):
                 z = np.where(y.ravel(), 1. / probs, -1. / (1. - probs)) 
                 z = np.clip(z, a_min=-8, a_max=8)
 
-            # fit SIM model
+            # fit SIM estimator
             param_grid = {"method": ["second_order", 'first_order']}
             grid = GridSearchCV(SIMRegressor(degree=2, knot_num=20, spline="a_spline", reg_lambda=0.1, reg_gamma=10,
                                   random_state=self.random_state), 
                           scoring={"mse": make_scorer(mean_squared_error, greater_is_better=False)}, refit=False,
                           cv=PredefinedSplit(val_fold), param_grid=param_grid, verbose=0, error_score=np.nan)
             # time
-            start = time.time()
             grid.fit(x, z, sample_weight=sample_weight)
-            model = grid.estimator.set_params(**grid.cv_results_['params'][np.where((grid.cv_results_['rank_test_mse'] == 1))[0][0]])
-            model.fit(x[idx1, :], z[idx1], sample_weight=sample_weight[idx1])
-            self.time_cost_ += time.time() - start
+            estimator = grid.estimator.set_params(**grid.cv_results_['params'][np.where((grid.cv_results_['rank_test_mse'] == 1))[0][0]])
+            estimator.fit(x[idx1, :], z[idx1], sample_weight=sample_weight[idx1])
 
             # stop criterion
-            pred_val_temp = pred_val + 0.5 * model.predict(x[idx2, :])
+            pred_val_temp = pred_val + 0.5 * estimator.predict(x[idx2, :])
             roc_auc_new = roc_auc_score(y[idx2], 1 / (1 + np.exp(-2 * pred_val_temp)))
             if roc_auc_opt < roc_auc_new:           
                 roc_auc_opt = roc_auc_new
@@ -274,26 +304,20 @@ class SIMLogitBoostClassifier(BaseSIMBooster, ClassifierMixin):
                 break
 
             # update
-            pred_train += 0.5 * model.predict(x[idx1, :])
-            pred_val += 0.5 * model.predict(x[idx2, :])
+            pred_train += 0.5 * estimator.predict(x[idx1, :])
+            pred_val += 0.5 * estimator.predict(x[idx2, :])
             probs = 1 / (1 + np.exp(-2 * np.hstack([pred_train, pred_val])))
-            self.sim_estimators_.append(model)
-            xgrid = np.linspace(model.shape_fit_.xmin, model.shape_fit_.xmax, 100).reshape([-1, 1])
-            ygrid = model.shape_fit_.predict(xgrid)
-            self.sim_importance_.append(np.std(ygrid))
-        
+            self.estimators_.append(estimator)
+
         self.tr_idx = idx1
         self.val_idx = idx2
-        self.importance_ratio_ = self.sim_importance_ / np.sum(self.sim_importance_)
-        self.betas_ = np.array([model.beta_.flatten() for model in self.sim_estimators_])
-        self.ortho_measure_ = np.linalg.norm(np.dot(self.betas_, self.betas_.T) - np.eye(self.betas_.shape[0]))
-
+        self.time_cost_ = time.time() - start
         return self
     
     def predict_proba(self, x):
 
-        pred_proba_inv = 0.5 * self._predict(x)
-        pred_prob = 1 / (1 + np.exp(-2 * pred_proba_inv))
+        pred = 0.5 * self.decision_function(x)
+        pred_prob = 1 / (1 + np.exp(-2 * pred))
         return pred_prob
 
     def predict(self, x):
@@ -326,6 +350,7 @@ class SIMAdaBoostClassifier(BaseSIMBooster, ClassifierMixin):
 
     def fit(self, x, y, sample_weight=None):
 
+        start = time.time()
         self._validate_hyperparameters()
         x, y = self._validate_input(x, y)
         n_samples, n_features = x.shape
@@ -340,43 +365,38 @@ class SIMAdaBoostClassifier(BaseSIMBooster, ClassifierMixin):
         val_fold = np.ones((len(indices)))
         val_fold[idx1] = -1
 
-        pred_train = 0 
-        pred_val = 0
-        probs = 0.5 * np.ones(n_samples)
-        sample_weight = 1 / n_samples * np.ones(n_samples)
-
         roc_auc_opt = -np.inf
-        self.time_cost_ = 0
-        self.sim_estimators_ = []
-        self.sim_importance_ = []
+        self.estimators_ = []
+        sample_weight = 1 / n_samples * np.ones(n_samples)
         for i in range(self.n_estimators):
 
             sample_weight = probs * (1 - probs)
             sample_weight /= np.sum(sample_weight)
             sample_weight = np.maximum(sample_weight, 2 * np.finfo(np.float64).eps)
 
-            # fit SIM model
+            # fit SIM estimator
             param_grid = {"method": ["second_order", 'first_order']}
             grid = GridSearchCV(SIMClassifier(degree=2, knot_num=20, spline="a_spline", reg_lambda=0.1, reg_gamma=10,
                                    random_state=self.random_state), 
                           scoring={"auc": make_scorer(roc_auc_score)}, refit=False,
                           cv=PredefinedSplit(val_fold), param_grid=param_grid, verbose=0, error_score=np.nan)
             # time
-            start = time.time()
             grid.fit(x, y, sample_weight=sample_weight)
-            model = grid.estimator.set_params(**grid.cv_results_['params'][np.where((grid.cv_results_['rank_test_mse'] == 1))[0][0]])
-            model.fit(x[idx1, :], y[idx1], sample_weight=sample_weight[idx1])
-            self.time_cost_ += time.time() - start
-
-            estimator_error = np.average((model.predict(x[idx1, :]) != y[idx1]), axis=0, weights=sample_weight)
-            if estimator_error <= 0:
-                break
+            estimator = grid.estimator.set_params(**grid.cv_results_['params'][np.where((grid.cv_results_['rank_test_auc'] == 1))[0][0]])
+            estimator.fit(x[idx1, :], y[idx1], sample_weight=sample_weight[idx1])
             
-            estimator_weight = np.log((1 - estimator_error) / estimator_error)
-            pred_val_temp = pred_val + model.predict(x[idx2, :])
-            roc_auc_new = roc_auc_score(y[idx2], 1 / (1 + np.exp(-2 * pred_val_temp)))
+            y_codes = np.array([-1., 1.])
+            y_coding = y_codes.take([0, 1] == y[:, np.newaxis])
+            with np.errstate(divide='ignore', over='ignore'):
+                sample_weight *= np.exp(-0.5 * np.sum(y_coding * np.log(np.hstack([1 - estimator.predict_proba(x),
+                                                             estimator.predict_proba(x)])), axis=1))
 
-            sample_weight = sample_weight * np.exp(estimator_weight * (pred_train != y[idx1]))
+            pred_proba = estimators_.predict_proba(x[idx2, :])
+            pred_proba = np.clip(pred_proba, np.finfo(pred_proba.dtype).eps, None)
+            log_proba = np.log(pred_proba)
+            pred_val = self.decision_function(x[idx2, :]) + (log_proba - (1. / 2) * log_proba.sum(axis=1)[:, np.newaxis])
+            pred_val_proba = 1 / (1 + np.exp(- pred_val))
+            roc_auc_new = roc_auc_score(y[idx2], pred_val_proba)
             # stop criterion
             if roc_auc_opt < roc_auc_new:           
                 roc_auc_opt = roc_auc_new
@@ -386,25 +406,29 @@ class SIMAdaBoostClassifier(BaseSIMBooster, ClassifierMixin):
 
             if early_stop_count >= self.early_stop_thres:
                 break
-
+            
             # update
-            self.sim_estimators_.append(model)
-            xgrid = np.linspace(model.shape_fit_.xmin, model.shape_fit_.xmax, 100).reshape([-1, 1])
-            ygrid = model.shape_fit_.predict(xgrid)
-            self.sim_importance_.append(np.std(ygrid))
+            self.estimators_.append(estimator)
         
         self.tr_idx = idx1
         self.val_idx = idx2
-        self.importance_ratio_ = self.sim_importance_ / np.sum(self.sim_importance_)
-        self.betas_ = np.array([model.beta_.flatten() for model in self.sim_estimators_])
-        self.ortho_measure_ = np.linalg.norm(np.dot(self.betas_, self.betas_.T) - np.eye(self.betas_.shape[0]))
-
+        self.time_cost_ = time.time() - start
         return self
     
+    def decision_function(self, x):
+
+        pred = 0
+        for i in self.estimators_:
+            pred_proba = estimators_.predict_proba(x)
+            pred_proba = np.clip(pred_proba, np.finfo(pred_proba.dtype).eps, None)
+            log_proba = np.log(pred_proba)
+            pred += (log_proba - (1. / 2) * log_proba.sum(axis=1)[:, np.newaxis])
+        return pred
+
     def predict_proba(self, x):
 
-        pred_proba_inv = 0.5 * self._predict(x)
-        pred_prob = 1 / (1 + np.exp(-2 * pred_proba_inv))
+        pred = self.decision_function(x)
+        pred_prob = 1 / (1 + np.exp(- pred))
         return pred_prob
 
     def predict(self, x):
