c756567e79747f629bea7b9c163e40d2b52c6f36
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-03-03 11:10:09 +0800

    update

--- pysim/aspline.py
+++ pysim/aspline.py
@@ -31,7 +31,7 @@ def diff_matrix(order, knot_num):
     
 class ASplineRegressor(BaseEstimator, RegressorMixin):
 
-    def __init__(self, knot_num=100, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99, maxiter=10):
+    def __init__(self, knot_num=20, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99, maxiter=10):
 
         self.knot_num = knot_num
         self.reg_gamma = reg_gamma
@@ -58,12 +58,12 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
         w = np.ones([self.knot_num], dtype=np.float32) 
         W = np.diag(w)
 
-        BB = np.tensordot(basis * sample_weight.reshape([-1, 1]), basis, axes=([0], [0]))
-        BY = np.tensordot(basis * sample_weight.reshape([-1, 1]), y, axes=([0], [0]))
+        BWB = np.tensordot(basis * sample_weight.reshape([-1, 1]), basis, axes=([0], [0]))
+        BWY = np.tensordot(basis * sample_weight.reshape([-1, 1]), y, axes=([0], [0]))
         for i in range(self.maxiter):
-            U = cholesky(BB + self.reg_gamma * np.dot(np.dot(D.T, W), D))
+            U = cholesky(BWB + self.reg_gamma * np.dot(np.dot(D.T, W), D))
             M = scipy.linalg.lapack.clapack.dtrtri(U)[0]
-            update_a = np.dot(np.dot(M, M.T.conj()), BY)
+            update_a = np.dot(np.dot(M, M.T.conj()), BWY)
             update_w = 1 / (np.dot(D, update_a) ** 2 + self.epsilon ** 2)
             W = np.diag(update_w.reshape([-1]))
 
@@ -72,9 +72,9 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
                {"x": [self.xmin, self.xmax], "knots": self.selected_knots_, "degree": self.degree})
         selected_basis = np.asarray(build_design_matrices([self.selected_xphi_.design_info],
                           {"x": x, "knots": self.selected_knots_, "degree": self.degree})[0])
-        seBB = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), selected_basis, axes=([0], [0]))
-        seBY = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), y, axes=([0], [0]))
-        self.coef_ = np.dot(np.linalg.pinv(seBB), seBY)
+        seBWB = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), selected_basis, axes=([0], [0]))
+        seBWY = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), y, axes=([0], [0]))
+        self.coef_ = np.dot(np.linalg.pinv(seBWB), seBWY)
         return self
 
     def predict(self, x):
@@ -91,8 +91,8 @@ class ASplineRegressor(BaseEstimator, RegressorMixin):
 
 class ASplineClassifier(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, knot_num=100, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99,
-                 maxiter=10, maxiter_irls=20):
+    def __init__(self, knot_num=20, reg_gamma=0.1, xmin=-1, xmax=1, degree=2, epsilon=0.00001, threshold=0.99,
+                 maxiter=10, maxiter_irls=10):
 
         self.knot_num = knot_num
         self.reg_gamma = reg_gamma
@@ -106,12 +106,20 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
         self.EPS = 10**(-5)
 
     def link(self, x):
-        return 1 / (1 + np.exp(-x))
+        with np.errstate(divide='ignore', over='ignore'):
+            return 1 / (1 + np.exp(-x))
 
     def inv_link(self, x):
-        return np.log(x) - np.log(1 - x)
+        with np.errstate(divide='ignore', over='ignore'):
+            return np.log(x) - np.log(1 - x)
     
     def fit(self, x, y, sample_weight=None):
+
+        n_samples = x.shape[0]
+        if sample_weight is None:
+            sample_weight = np.ones(n_samples)
+        else:
+            sample_weight = sample_weight * n_samples
 
         knots = list(np.linspace(self.xmin, self.xmax, self.knot_num + 2, dtype=np.float32)[1:-1])
         xphi = dmatrix("bs(x, knots = knots, degree=degree, include_intercept=True) - 1",
@@ -125,7 +133,9 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
         tempy = y.copy()
         tempy[tempy==0] = 0.01
         tempy[tempy==1] = 0.99
-        update_a = np.dot(np.linalg.pinv(np.dot(init_basis.T, init_basis)), np.dot(init_basis.T, self.inv_link(tempy)))
+        BWB = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), init_basis, axes=([0], [0]))
+        BWY = np.tensordot(init_basis * sample_weight.reshape([-1, 1]), self.inv_link(tempy), axes=([0], [0]))
+        update_a = np.dot(np.linalg.pinv(BWB), BWY)
         for i in range(self.maxiter):
             tempy = y.copy()
             basis = init_basis.copy()
@@ -139,16 +149,10 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
                 if np.sum(mask) == 0:
                     break
 
-                tempy = tempy[mask] 
-                lp = lp[mask] 
-                mu = mu[mask] 
-                omega = omega[mask] 
-                basis = basis[mask,:]
-            
-                BB = np.tensordot(basis * omega.reshape([-1, 1]), basis, axes=([0], [0]))
-                left = np.linalg.pinv(BB + self.reg_gamma * D.T.dot(W).dot(D))
-                right = BB.dot(update_a) + basis.T.dot(tempy - mu)
-                update_a = np.dot(left, right)
+                BW = basis[mask, :] * sample_weight[mask, :].reshape([-1, 1])
+                BWOB = np.tensordot(BW * omega[mask].reshape([-1, 1]), basis[mask, :], axes=([0], [0]))
+                update_a = np.dot(np.linalg.pinv(BWOB + self.reg_gamma * D.T.dot(W).dot(D)),
+                            BWOB.dot(update_a) + np.tensordot(BW, tempy[mask] - mu[mask], axes=([0], [0])))
             update_w = 1 / (np.dot(D, update_a) ** 2 + self.epsilon ** 2)
             W = np.diag(update_w.reshape([-1]))
 
@@ -159,27 +163,23 @@ class ASplineClassifier(BaseEstimator, ClassifierMixin):
                           {"x": x, "knots": self.selected_knots_, "degree": self.degree})[0])
 
         tempy = y.copy()
-        basis = selected_basis.copy()
         tempy[tempy==0] = 0.01
         tempy[tempy==1] = 0.99
-        self.coef_ = np.dot(np.linalg.pinv(np.dot(basis.T, basis)), np.dot(basis.T, self.inv_link(tempy)))
+        seBWB = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), selected_basis, axes=([0], [0]))
+        seBWY = np.tensordot(selected_basis * sample_weight.reshape([-1, 1]), self.inv_link(tempy), axes=([0], [0]))
+        update_a = np.dot(np.linalg.pinv(seBWB), seBWY)
         for j in range(self.maxiter_irls):
-            lp = np.dot(basis, self.coef_)
+            lp = np.dot(selected_basis, self.coef_)
             mu = self.link(lp)
             omega = mu * (1 - mu)
             mask = (np.abs(omega) >= self.EPS) * np.isfinite(omega)
             mask = mask.ravel()
             if np.sum(mask) == 0:
                 break
-            tempy = tempy[mask] 
-            lp = lp[mask] 
-            mu = mu[mask] 
-            omega = omega[mask]
-            basis = basis[mask,:]
-            BB = np.tensordot(basis * omega.reshape([-1, 1]), basis, axes=([0], [0]))
-            left_ = np.linalg.pinv(BB)
-            right = BB.dot(self.coef_) + basis.T.dot(tempy - mu)
-            self.coef_ = left_.dot(right)
+            seBW = selected_basis[mask, :] * sample_weight[mask, :].reshape([-1, 1])
+            seBWOB = np.tensordot(seBW * omega[mask].reshape([-1, 1]), selected_basis[mask, :], axes=([0], [0]))
+            self.coef_ = np.dot(np.linalg.pinv(seBWOB),
+                          seBWOB.dot(self.coef_) + np.tensordot(seBW, tempy[mask] - mu[mask], axes=([0], [0])))
         return self
     
     def predict(self, x):
