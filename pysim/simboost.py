29c41b24d9210f61a1f45be6cc3c91c87621c362
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-03-07 20:00:48 +0800

    remove early stopping

--- pysim/simboost.py
+++ pysim/simboost.py
@@ -194,10 +194,10 @@ class SimBoostRegressor(BaseSimBooster, RegressorMixin):
 
         fig = plt.figure(figsize=(6, 4))
         plt.plot(np.arange(1, len(self.estimator_val_mse) + 1, 1), self.estimator_val_mse)
-        plt.axvline(np.argmin(self.estimator_val_mse), linestyle="dotted", color="red")
+        plt.axvline(np.argmin(self.estimator_val_mse) + 1, linestyle="dotted", color="red")
         plt.axvline(len(self.best_estimators_), linestyle="dotted", color="red")
         plt.plot(np.argmin(self.estimator_val_mse) + 1, np.min(self.estimator_val_mse), "*", markersize=12, color="red")
-        plt.plot(len(self.best_estimators_), self.estimator_val_auc[len(self.best_estimators_) - 1], "o", markersize=8, color="red")
+        plt.plot(len(self.best_estimators_), self.estimator_val_mse[len(self.best_estimators_) - 1], "o", markersize=8, color="red")
         plt.xlabel("Number of Estimators", fontsize=12)
         plt.ylabel("Validation AUC", fontsize=12)
         plt.xlim(0.5, len(self.estimator_val_mse) + 0.5)
@@ -303,7 +303,7 @@ class SimLogitBoostClassifier(BaseSimBooster, ClassifierMixin):
 
         fig = plt.figure(figsize=(6, 4))
         plt.plot(np.arange(1, len(self.estimator_val_auc) + 1, 1), self.estimator_val_auc)
-        plt.axvline(np.argmax(self.estimator_val_auc), linestyle="dotted", color="red")
+        plt.axvline(np.argmax(self.estimator_val_auc) + 1, linestyle="dotted", color="red")
         plt.axvline(len(self.best_estimators_), linestyle="dotted", color="red")
         plt.plot(np.argmax(self.estimator_val_auc) + 1, np.max(self.estimator_val_auc), "*", markersize=12, color="red")
         plt.plot(len(self.best_estimators_), self.estimator_val_auc[len(self.best_estimators_) - 1], "o", markersize=8, color="red")
@@ -432,7 +432,7 @@ class SimAdaBoostClassifier(BaseSimBooster, ClassifierMixin):
 
         fig = plt.figure(figsize=(6, 4))
         plt.plot(np.arange(1, len(self.estimator_val_auc) + 1, 1), self.estimator_val_auc)
-        plt.axvline(np.argmax(self.estimator_val_auc), linestyle="dotted", color="red")
+        plt.axvline(np.argmax(self.estimator_val_auc) + 1, linestyle="dotted", color="red")
         plt.axvline(len(self.best_estimators_), linestyle="dotted", color="red")
         plt.plot(np.argmax(self.estimator_val_auc) + 1, np.max(self.estimator_val_auc), "*", markersize=12, color="red")
         plt.plot(len(self.best_estimators_), self.estimator_val_auc[len(self.best_estimators_) - 1], "o", markersize=8, color="red")
