46e30ec698085801ee95ba9e85a0d4f45729d841
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-05-26 11:51:55 +0800

    learning_rate=1 for sim1; add API to trun off pruning

--- pysim/simboost.py
+++ pysim/simboost.py
@@ -859,7 +859,7 @@ class SimBoostRegressor(BaseSimBooster, RegressorMixin):
                         sample_weight=sample_weight, proj_mat=proj_mat, method=self.inner_update,
                         n_inner_iter_no_change=1, batch_size=min(200, int(0.2 * n_samples)), val_ratio=self.val_ratio)
             # update
-            z = z - self.learning_rate[indice] * sim_estimator.predict(x)
+            z = z - self.learning_rates[indice] * sim_estimator.predict(x)
             self.sim_estimators_.append(sim_estimator)
 
     def _pruning(self, x, y):
